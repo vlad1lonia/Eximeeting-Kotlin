38d4898178b7b413a166cb2630ea92b125557469
vladcelona <vladbalandin2013@gmail.com>
2023-03-29 23:43:54 +0300

    Updates Event.kt dataclass (Added new parameters. Also added function that converts eventStatusCode to String). Minor code and design updates (30/03/2023)

--- app/src/main/java/com/application/vladcelona/eximeeting/data_classes/Event.kt
+++ app/src/main/java/com/application/vladcelona/eximeeting/data_classes/Event.kt
@@ -1,8 +1,39 @@
 package com.application.vladcelona.eximeeting.data_classes
 
-import java.util.Date
-import java.util.UUID
+import android.util.Log
+import java.util.*
 
-data class Event(val id: UUID = UUID.randomUUID(), var title: String = "",
-                 var fromDate: Date = Date(), var toDate: Date = Date(),
-                 var location: String = "", var eventStatus: Short = 0)
+private const val TAG = "Event"
+data class Event(
+            // Event id
+            val id: UUID = UUID.randomUUID(),
+
+            // Data shown in Event RecyclerView
+            val eventName: String = "",
+            val fromDate: Date = Date(), val toDate: Date = Date(),
+            val location: String = "", val address: String = "",
+            val organizer: String = "", val eventStatusCode: Int = 0,
+
+            // Data shown when user presses on a certain event
+            var title: String = "", var description: String = "",
+            val speakers: ArrayList<String> = ArrayList(),
+            val moderators: ArrayList<String> = ArrayList(),
+
+            // TODO: Add business programme and map (also add for pavilions if possible)
+){
+
+    // Function for converting eventStatusCode to the String (Text)
+    fun convertEventStatus(): String {
+        return when (eventStatusCode) {
+            0 -> "Starts soon" // If the event starts within a week
+            1 -> "First day"
+            2 -> "Ongoing" // If it is not the first or last day of the event
+            3 -> "Last day"
+            4 -> "Ended"
+            else -> {
+                Log.i(TAG, "Error while getting eventStatusCode")
+                ""
+            }
+        }
+    }
+}
